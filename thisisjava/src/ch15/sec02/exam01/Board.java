6af2085b822c53e90e8a9d120f1ed6ca24cf017b
KOSA <KOSA@DESKTOP-LUBR8UB>
2024-02-13 15:15:42 +0900

    롬복을 사용하여 수정함

--- thisisjava/src/ch15/sec02/exam01/Board.java
+++ thisisjava/src/ch15/sec02/exam01/Board.java
@@ -2,52 +2,61 @@ package ch15.sec02.exam01;
 
 import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Board {
 	private int    id;
 	private String subject;
 	private String content;
 	private String writer;
 	
-	public Board(int id, String subject, String content, String writer) {
-		super();
-		this.id = id;
-		this.subject = subject;
-		this.content = content;
-		this.writer = writer;
+	public Board(int id) {
+		this(id, "", "", "");
+//		this.id = id;
+//		this.subject = "";
+//		this.content = "";
+//		this.writer = "";
 	}
-
-	public Board(int i) {
-		this(i, "", "", "");
-	}
-
-	public String getSubject() {
-		return subject;
-	}
-
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getWriter() {
-		return writer;
-	}
-
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-
-	public int getId() {
-		return id;
-	}
-	
+//
+//	public Board(int id, String subject, String content, String writer) {
+//		super();
+//		this.id = id;
+//		this.subject = subject;
+//		this.content = content;
+//		this.writer = writer;
+//	}
+//
+//	public String getSubject() {
+//		return subject;
+//	}
+//
+//	public void setSubject(String subject) {
+//		this.subject = subject;
+//	}
+//
+//	public String getContent() {
+//		return content;
+//	}
+//
+//	public void setContent(String content) {
+//		this.content = content;
+//	}
+//
+//	public String getWriter() {
+//		return writer;
+//	}
+//
+//	public void setWriter(String writer) {
+//		this.writer = writer;
+//	}
+//
+//	public int getId() {
+//		return id;
+//	}
+//	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -64,11 +73,11 @@ public class Board {
 		Board other = (Board) obj;
 		return id == other.id;
 	}
-
-	@Override
-	public String toString() {
-		return "Board [id=" + id + ", subject=" + subject + ", content=" + content + ", writer=" + writer + "]";
-	}
+//
+//	@Override
+//	public String toString() {
+//		return "Board [id=" + id + ", subject=" + subject + ", content=" + content + ", writer=" + writer + "]";
+//	}
 	
 	
 }
